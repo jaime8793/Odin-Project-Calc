8679f6715db5edcf3df0b23a3f08e55d7de2b460
jaime <jamesowiti24@gmail.com>
2025-03-27 16:09:38 +0300

    Connected to MongoDb Succesfully

--- index.mjs
+++ index.mjs
@@ -3,6 +3,18 @@ import "dotenv/config";
 import mongoose from "mongoose";
 
 const app = express();
+const port = process.env.port;
+const mongoDBURI = process.env.mongoDBURI;
+
+try {
+  await mongoose.connect(mongoDBURI);
+  console.log(`connected to the DB`);
+} catch (error) {
+  console.log(`MongoDB refused to connect ${error}`);
+}
+
+
+
 
 app.get("/", (req, res) => {
   res.sendStatus(200);
@@ -11,6 +23,6 @@ app.get("/nicknames", (req, res) => {
   res.send(["Ammo", "Dibo", "Fujo"]);
 });
 
-app.listen(process.env.port, () => {
-  console.log(`Sever running on port ${process.env.port}`);
+app.listen(port, () => {
+  console.log(`Sever running on port ${port}`);
 });
